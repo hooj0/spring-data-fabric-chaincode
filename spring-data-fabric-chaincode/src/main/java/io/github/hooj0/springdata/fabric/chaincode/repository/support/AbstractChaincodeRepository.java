17dcd10a7fe33ab8c6726e134743a8e9fa6954bd
hoojo <hoojo@qq.com>
2018-07-18 18:40:27 +0800

    :sparkles: :bento: Added chaincode abstract repository

---
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/AbstractChaincodeRepository.java
@@ -0,0 +1,73 @@
+package io.github.hooj0.springdata.fabric.chaincode.repository.support;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import io.github.hooj0.springdata.fabric.chaincode.core.ChaincodeOperations;
+import io.github.hooj0.springdata.fabric.chaincode.repository.ChaincodeRepository;
+import lombok.NoArgsConstructor;
+
+/**
+ * ChaincodeRepository Base abstract repository
+ * @author hoojo
+ * @createDate 2018年7月18日 上午9:18:34
+ * @file AbstractChaincodeRepositoryQuery.java
+ * @package io.github.hooj0.springdata.fabric.chaincode.repository.support
+ * @project spring-data-fabric-chaincode
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@NoArgsConstructor
+public abstract class AbstractChaincodeRepository<T> implements ChaincodeRepository<T> {
+
+	protected ChaincodeEntityInformation<T, ?> entityInformation;
+	protected ChaincodeOperations operations;
+	
+	protected Class<T> entityClass;
+	
+	public AbstractChaincodeRepository(ChaincodeOperations operations) {
+		this.operations = operations;
+	}
+	
+	public AbstractChaincodeRepository(ChaincodeEntityInformation<T, ?> entityInformation, ChaincodeOperations operations) {
+		this.entityInformation = entityInformation;
+		this.operations = operations;
+	}
+	
+	@Override
+	public Class<T> getEntityClass() {
+		if (!isEntityClassSet()) {
+			try {
+				this.entityClass = resolveReturnedClassFromGenericType();
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to resolve EntityClass. Please use according setter!", e);
+			}
+		}
+		return entityClass;
+	}
+	
+	private boolean isEntityClassSet() {
+		return entityClass != null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Class<T> resolveReturnedClassFromGenericType() {
+		ParameterizedType parameterizedType = resolveReturnedClassFromGenericType(getClass());
+		return (Class<T>) parameterizedType.getActualTypeArguments()[0];
+	}
+	
+	private ParameterizedType resolveReturnedClassFromGenericType(Class<?> clazz) {
+		Object genericSuperclass = clazz.getGenericSuperclass();
+		
+		if (genericSuperclass instanceof ParameterizedType) {
+			
+			ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+			Type rawtype = parameterizedType.getRawType();
+			if (SimpleChaincodeRepository.class.equals(rawtype)) {
+				return parameterizedType;
+			}
+		}
+		return resolveReturnedClassFromGenericType(clazz.getSuperclass());
+	}
+}
