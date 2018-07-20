2b27471d9e85fabb936532938b18cd1e15ec1aea
hoojo <hoojo@qq.com>
2018-07-20 10:28:47 +0800

    :sparkles: :bento: Added chaincode `template` support

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/ChaincodeTemplate.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/ChaincodeTemplate.java
@@ -1,9 +1,13 @@
 package io.github.hooj0.springdata.fabric.chaincode.core;
 
+import org.hyperledger.fabric.sdk.HFClient;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.mapping.context.MappingContext;
+
+import io.github.hooj0.springdata.fabric.chaincode.core.convert.ChaincodeConverter;
+import io.github.hooj0.springdata.fabric.chaincode.core.convert.MappingChaincodeConverter;
 
 /**
  * <b>function:</b> Chaincode Operations Template
@@ -16,15 +20,52 @@ import org.springframework.context.ApplicationContextAware;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public class ChaincodeTemplate implements /*ChaincodeOperations,*/ ApplicationContextAware, InitializingBean {
+public class ChaincodeTemplate implements ChaincodeOperations, ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+	private MappingContext mappingContext;
+	private ChaincodeConverter converter;
+	private HFClient client;
+	
+	public ChaincodeTemplate() {
+		this(newDefaultConverter());
+	}
+	
+	public ChaincodeTemplate(ChaincodeConverter converter) {
+		this.converter = converter;
+		this.mappingContext = converter.getMappingContext();
+		
+		this.client = HFClient.createNewInstance();
+	}
 
 	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void install() {
+		
+	}
+	
+	@Override
+	public HFClient getClient() {
+		return this.client;
+	}
 
+	@Override
+	public ChaincodeConverter getConverter() {
+		return this.converter;
 	}
 
 	@Override
 	public void setApplicationContext(ApplicationContext context) throws BeansException {
-
+		this.applicationContext = context;
+		
+		if (converter instanceof ApplicationContextAware) {
+			((ApplicationContextAware) converter).setApplicationContext(context);
+		}
+	}
+	
+	private static MappingChaincodeConverter newDefaultConverter() {
+		MappingChaincodeConverter converter = new MappingChaincodeConverter();
+		converter.afterPropertiesSet();
+		
+		return converter;
 	}
 }
