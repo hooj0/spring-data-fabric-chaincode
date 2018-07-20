fd34909dbfe30c43d74f4772df8c31ff6ede7d97
hoojo <hoojo@qq.com>
2018-07-20 18:17:47 +0800

    :sparkles: :bento: Added repository implements chaincode `invoke & query & deploy` options

---
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/options/InstallOptions.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.springdata.fabric.chaincode.core.options;
+
+/**
+ * <b>function:</b>
+ * @author hoojo
+ * @createDate 2018年7月20日 下午4:46:50
+ * @file InstallOptions.java
+ * @package io.github.hooj0.springdata.fabric.chaincode.core.options
+ * @project spring-data-fabric-chaincode
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class InstallOptions {
+
+}
