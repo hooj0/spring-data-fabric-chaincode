7640b3572ca4bfc5e1d0034cdccb590c631bfec2
hoojo <hoojo@qq.com>
2018-07-29 22:22:31 +0800

    :sparkles: :bento: :tada:  Init add chaincode operations `transaction & deploy` bean cache

---
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/cache/ChaincodeOperationBeanCache.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.springdata.fabric.chaincode.core.cache;
+
+/**
+ * chaincode operations `transaction & deploy` bean cache
+ * @author hoojo
+ * @createDate 2018年7月29日 下午4:37:58
+ * @file ChaincodeOperationBeanCache.java
+ * @package io.github.hooj0.springdata.fabric.chaincode.core.cache
+ * @project spring-data-fabric-chaincode
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class ChaincodeOperationBeanCache {
+
+}
