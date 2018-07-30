d01d235291f7b4166969b0842dcce4db78abd911
hoojo <hoojo@qq.com>
2018-07-30 18:41:48 +0800

    :sparkles: :bento: :recycle:  Changed chaincode operations `transaction & deploy` bean cache

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/cache/ChaincodeOperationBeanCache.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/cache/ChaincodeOperationBeanCache.java
@@ -1,5 +1,16 @@
 package io.github.hooj0.springdata.fabric.chaincode.core.cache;
 
+import java.util.Map;
+
+import org.springframework.util.Assert;
+
+import com.google.common.collect.Maps;
+
+import io.github.hooj0.fabric.sdk.commons.core.ChaincodeDeployOperations;
+import io.github.hooj0.fabric.sdk.commons.core.ChaincodeTransactionOperations;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.Criteria;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * chaincode operations `transaction & deploy` bean cache
  * @author hoojo
@@ -11,6 +22,53 @@ package io.github.hooj0.springdata.fabric.chaincode.core.cache;
  * @email hoojo_@126.com
  * @version 1.0
  */
+@Slf4j
 public class ChaincodeOperationBeanCache {
 
+	private Map<String, ChaincodeDeployOperations> deployOperationsCache = Maps.newConcurrentMap();
+	private Map<String, ChaincodeTransactionOperations> transactionOperationsCache = Maps.newConcurrentMap();
+	
+	public void setDeployOperationsCache(Criteria criteria, ChaincodeDeployOperations deployOperations) {
+		Assert.notNull(deployOperations, "ChaincodeDeployOperations cache bean is null.");
+		
+		if (!deployOperationsCache.containsKey(getKey(criteria))) {
+			deployOperationsCache.put(getKey(criteria), deployOperations);
+		} else {
+			log.warn("ChaincodeDeployOperations beans key '{}' is contains.", getKey(criteria));
+		}
+	}
+	
+	public ChaincodeDeployOperations getDeployOperationCache(Criteria criteria) {
+		return deployOperationsCache.get(getKey(criteria));
+	}
+	
+	public boolean containsDeployOperations(Criteria criteria) {
+		return deployOperationsCache.containsKey(getKey(criteria));
+	}
+	
+	public void setTransactionOperationsCache(Criteria criteria, ChaincodeTransactionOperations transactionOperations) {
+		Assert.notNull(transactionOperations, "ChaincodeTransactionOperations cache bean is null.");
+		
+		if (!transactionOperationsCache.containsKey(getKey(criteria))) {
+			transactionOperationsCache.put(getKey(criteria), transactionOperations);
+		} else {
+			log.warn("ChaincodeTransactionOperations beans key '{}' is contains.", getKey(criteria));
+		}
+	}
+	
+	public ChaincodeTransactionOperations getTransactionOperationCache(Criteria criteria) {
+		return transactionOperationsCache.get(getKey(criteria));
+	}
+	
+	public boolean containsTransactionOperations(Criteria criteria) {
+		return transactionOperationsCache.containsKey(getKey(criteria));
+	}
+	
+	private String getKey(Criteria criteria) {
+		Assert.notNull(criteria, "Criteria is not null!");
+		Assert.hasText(criteria.getChannel(), "Criteria.channel property is null!");
+		Assert.hasText(criteria.getOrg(), "Criteria.org property is null!");
+		
+		return criteria.getChannel() + "_" + criteria.getOrg();
+	}
 }
