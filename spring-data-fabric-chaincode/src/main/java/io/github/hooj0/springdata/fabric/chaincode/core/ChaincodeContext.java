e8698cee31d54aa55846b7f9ecbb36e7a25098d8
hoojo <hoojo@qq.com>
2018-07-30 09:03:23 +0800

    :sparkles: :bento: :tada:  Init add add context

---
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/ChaincodeContext.java
@@ -0,0 +1,17 @@
+package io.github.hooj0.springdata.fabric.chaincode.core;
+
+/**
+ * 
+ * @author hoojo
+ * @createDate 2018年7月19日 下午6:26:02
+ * @file ChaincodeContext.java
+ * @package io.github.hooj0.springdata.fabric.chaincode.core
+ * @project spring-data-fabric-chaincode
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class ChaincodeContext extends Object {
+
+	
+}
