00244503f5827725a4366d7f2b84c73798519d04
hoojo <hoojo@qq.com>
2018-07-30 18:41:58 +0800

    :sparkles: :bento: :recycle:  Changed 部署智能合约 Chaincode Repository

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/DeployChaincodeRepository.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/DeployChaincodeRepository.java
@@ -2,8 +2,15 @@ package io.github.hooj0.springdata.fabric.chaincode.repository;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.concurrent.CompletableFuture;
 
+import org.hyperledger.fabric.sdk.BlockEvent.TransactionEvent;
+import org.hyperledger.fabric.sdk.ProposalResponse;
 import org.springframework.data.repository.NoRepositoryBean;
+
+import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 
 /**
  * 部署智能合约 Chaincode Repository
@@ -19,13 +26,22 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface DeployChaincodeRepository<T> extends ChaincodeRepository<T> {
 
+	// install
+	/**
+	 * 安装智能合约 Chaincode 
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午3:58:26
+	 * @param chaincodeSourceLocation 智能合约源码文件位置
+	 */
+	Collection<ProposalResponse> install(String chaincodeSourceLocation);
+
 	/**
 	 * 安装智能合约 Chaincode 
 	 * @author hoojo
 	 * @createDate 2018年7月20日 下午3:58:26
 	 * @param chaincodeSourceFile 智能合约源码文件
 	 */
-	public void install(File chaincodeSourceFile);
+	Collection<ProposalResponse> install(File chaincodeSourceFile);
 	
 	/**
 	 * 安装智能合约 Chaincode 
@@ -33,66 +49,175 @@ public interface DeployChaincodeRepository<T> extends ChaincodeRepository<T> {
 	 * @createDate 2018年7月20日 下午3:59:08
 	 * @param chaincodeInputStream 智能合约源码文件Stream
 	 */
-	public void install(InputStream chaincodeInputStream);
+	Collection<ProposalResponse> install(InputStream chaincodeInputStream);
+	
+	// instantiate
+	
+	/**
+	 * 实例化智能合约 Chaincode
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 */
+	ResultSet instantiate(String func);
+	
+	/**
+	 * 实例化智能合约 Chaincode
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	ResultSet instantiate(String func, Object... args);
 
 	/**
 	 * 实例化智能合约 Chaincode
 	 * @author hoojo
-	 * @createDate 2018年7月20日 下午3:57:47
-	 * @param policyAsBytes 背书策略配置文件 bytes
+	 * @createDate 2018年7月20日 下午4:03:40
 	 * @param func 智能合约Chaincode初始化方法
 	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String instantiate(byte[] policyAsBytes, String func, String... args);
+	ResultSet instantiate(String func, LinkedHashMap<String, Object> args);
+	
+	// instantiate async
 	
 	/**
-	 * 实例化智能合约 Chaincode
+	 * 实例化智能合约 Chaincode 返回异步线程模型
 	 * @author hoojo
 	 * @createDate 2018年7月20日 下午4:03:40
-	 * @param policyFile 背书策略配置文件
 	 * @param func 智能合约Chaincode初始化方法
-	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String instantiate(File policyFile, String func, String... args);
+	CompletableFuture<TransactionEvent> instantiateAsync(String func);
 	
 	/**
-	 * 实例化智能合约 Chaincode
+	 * 实例化智能合约 Chaincode 返回异步线程模型
 	 * @author hoojo
-	 * @createDate 2018年7月20日 下午4:05:22
-	 * @param policyInputStream 背书策略配置文件输入流
+	 * @createDate 2018年7月20日 下午4:03:40
 	 * @param func 智能合约Chaincode初始化方法
 	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String instantiate(InputStream policyInputStream, String func, String... args);
+	CompletableFuture<TransactionEvent> instantiateAsync(String func, Object... args);
+
+	/**
+	 * 实例化智能合约 Chaincode 返回异步线程模型
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	CompletableFuture<TransactionEvent> instantiateAsync(String func, LinkedHashMap<String, Object> args);
 	
+	// instantiate async return event
+	
+	/**
+	 * 实例化智能合约 Chaincode 返回交易事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 */
+	TransactionEvent instantiateFor(String func);
+	
+	/**
+	 * 实例化智能合约 Chaincode 返回交易事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	TransactionEvent instantiateFor(String func, Object... args);
+
+	/**
+	 * 实例化智能合约 Chaincode 返回交易事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	TransactionEvent instantiateFor(String func, LinkedHashMap<String, Object> args);
+	
+	
+	
+	// upgrade
 	/**
 	 * 升级智能合约 Chaincode
 	 * @author hoojo
-	 * @createDate 2018年7月20日 下午3:57:47
-	 * @param policyAsBytes 背书策略配置文件 bytes
+	 * @createDate 2018年7月20日 下午4:03:40
 	 * @param func 智能合约Chaincode初始化方法
-	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String upgrade(byte[] policyAsBytes, String version, String func, String... args);
+	ResultSet upgrade(String func);
 	
 	/**
 	 * 升级智能合约 Chaincode
 	 * @author hoojo
 	 * @createDate 2018年7月20日 下午4:03:40
-	 * @param policyFile 背书策略配置文件
 	 * @param func 智能合约Chaincode初始化方法
 	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String upgrade(File policyFile, String version, String func, String... args);
-	
+	ResultSet upgrade(String func, Object... args);
+
 	/**
 	 * 升级智能合约 Chaincode
 	 * @author hoojo
-	 * @createDate 2018年7月20日 下午4:05:22
-	 * @param policyInputStream 背书策略配置文件输入流
+	 * @createDate 2018年7月20日 下午4:03:40
 	 * @param func 智能合约Chaincode初始化方法
 	 * @param args 智能合约Chaincode初始化方法参数
 	 */
-	public String upgrade(InputStream policyInputStream, String version, String func, String... args);
+	ResultSet upgrade(String func, LinkedHashMap<String, Object> args);
+	
+	// upgrade async
+	
+	/**
+	 * 升级智能合约 Chaincode，返回异步线程模型
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 */
+	CompletableFuture<TransactionEvent> upgradeAsync(String func);
+	
+	/**
+	 * 升级智能合约 Chaincode，返回异步线程模型
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	CompletableFuture<TransactionEvent> upgradeAsync(String func, Object... args);
+
+	/**
+	 * 升级智能合约 Chaincode，返回异步线程模型
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	CompletableFuture<TransactionEvent> upgradeAsync(String func, LinkedHashMap<String, Object> args);
+	
+	// upgrade async return event
+	/**
+	 * 升级智能合约 Chaincode，返回事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	TransactionEvent upgradeFor(String func);
+	
+	/**
+	 * 升级智能合约 Chaincode，返回事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	TransactionEvent upgradeFor(String func, Object... args);
+
+	/**
+	 * 升级智能合约 Chaincode，返回事件结果
+	 * @author hoojo
+	 * @createDate 2018年7月20日 下午4:03:40
+	 * @param func 智能合约Chaincode初始化方法
+	 * @param args 智能合约Chaincode初始化方法参数
+	 */
+	TransactionEvent upgradeFor(String func, LinkedHashMap<String, Object> args);
 	
 }
