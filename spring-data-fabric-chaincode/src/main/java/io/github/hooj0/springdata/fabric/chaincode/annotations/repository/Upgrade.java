e21357787971f74c356be1f35aa302e3661e960f
hoojo <hoojo@qq.com>
2018-07-31 18:51:40 +0800

    :sparkles: :bento: :recycle:  Changed <b>function:</b> 升级 智能合约 Chaincode

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Upgrade.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Upgrade.java
@@ -32,6 +32,36 @@ public @interface Upgrade {
 	@AliasFor(annotation = Proposal.class)
 	String value() default "";
 	
-	/** 部署升级的版本号 */
-	String version() default "";
+	/** 执行Chaincode智能合约的方法名称，默认为当前注解方法的名称 */
+	String func() default "";
+	
+	/** 执行Chaincode智能合约的参数，支持占位符或spel表达式，默认取参数列表  */
+	@AliasFor(annotation = Proposal.class, attribute = "args")
+	String[] args() default {};
+	
+	/** 背书文件位置  */
+	String endorsementPolicyFile();
+	
+	/** HFClient 客户端上下文用户  */
+	@AliasFor(annotation = Proposal.class, attribute = "clientUser")
+	String clientUser() default "";
+	
+	/** 当前请求用户  */
+	@AliasFor(annotation = Proposal.class, attribute = "requestUser")
+	String requestUser() default "";
+	
+	/** 发送给特定的 peer节点 */
+	@AliasFor(annotation = Proposal.class, attribute = "specificPeers")
+	boolean specificPeers() default false;
+	
+	/** 请求提议等待响应事件 */
+	@AliasFor(annotation = Proposal.class, attribute = "waitTime")
+	long proposalWaitTime() default 0;
+	
+	/** 交易等待时间 */
+	@AliasFor(annotation = Transaction.class, attribute = "waitTime")
+	long transactionWaitTime() default 0;
+	/** 发起交易的用户 */
+	@AliasFor(annotation = Transaction.class, attribute = "user")
+	String transactionsUser() default "";
 }
