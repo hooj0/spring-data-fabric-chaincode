025635ccbbf401c4eb1ba1f2b38482e1328fbe4a
hoojo <hoojo@qq.com>
2018-07-31 18:51:37 +0800

    :sparkles: :bento: :recycle:  Changed Chaincode智能合约 交易请求

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Transaction.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Transaction.java
@@ -1,5 +1,14 @@
 package io.github.hooj0.springdata.fabric.chaincode.annotations.repository;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.data.annotation.QueryAnnotation;
+
 /**
  * Chaincode智能合约 交易请求
  * @author hoojo
@@ -11,6 +20,16 @@ package io.github.hooj0.springdata.fabric.chaincode.annotations.repository;
  * @email hoojo_@126.com
  * @version 1.0
  */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Documented
+@QueryAnnotation
 public @interface Transaction {
 
+	/** 交易等待时间 */
+	long waitTime() default 0;
+	
+	/** 发起交易的用户 */
+	String user() default "";
 }
