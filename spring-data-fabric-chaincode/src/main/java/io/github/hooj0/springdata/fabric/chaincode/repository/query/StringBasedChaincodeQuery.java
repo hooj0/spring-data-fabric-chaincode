c7b26618dbf015d6122e755bc3ad15b0394233c9
hoojo <hoojo@qq.com>
2018-08-02 08:43:45 +0800

    :sparkles: :bento: :recycle:  Changed Chaincode Query of type String, mostly used for annotation configuration query

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/query/StringBasedChaincodeQuery.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/query/StringBasedChaincodeQuery.java
@@ -1,19 +1,38 @@
 package io.github.hooj0.springdata.fabric.chaincode.repository.query;
 
-import java.util.Collection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.File;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.repository.query.ParametersParameterAccessor;
 import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
+import org.springframework.data.repository.query.ResultProcessor;
+import org.springframework.data.repository.query.ReturnedType;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
+import com.google.common.base.Optional;
+
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Install;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Instantiate;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Invoke;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Proposal;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Query;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Transaction;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Upgrade;
 import io.github.hooj0.springdata.fabric.chaincode.core.ChaincodeOperations;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.InstallCriteria;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.InstantiateCriteria;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.InvokeCriteria;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.QueryCriteria;
+import io.github.hooj0.springdata.fabric.chaincode.core.query.UpgradeCriteria;
+import io.github.hooj0.springdata.fabric.chaincode.repository.query.parser.ExpressionEvaluatingParameterBinder;
+import io.github.hooj0.springdata.fabric.chaincode.repository.query.parser.SimpleStatement;
+import io.github.hooj0.springdata.fabric.chaincode.repository.query.parser.StringBasedQueryBinder;
+import io.github.hooj0.springdata.fabric.chaincode.repository.query.parser.StringBasedQueryParser;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * String 类型的 Chaincode Query，多用于注解配置的查询方式
+ * @changelog Chaincode Query of type String, mostly used for annotation configuration query
  * @author hoojo
  * @createDate 2018年7月18日 下午3:26:07
  * @file StringBasedChaincodeQuery.java
@@ -26,9 +45,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StringBasedChaincodeQuery extends AbstractChaincodeQuery {
 
-	private static final Pattern PARAMETER_PLACEHOLDER = Pattern.compile("\\?(\\d+)");
-	private static final String QUERY_ARGS_SEPARATOR = "|,|";
+	private static final String QUERY_ARGS_SEPARATOR = "_;_";
 
+	private final StringBasedQueryBinder binder;
+	private final StringBasedQueryParser parser;
 	private String query;
 	
 	public StringBasedChaincodeQuery(ChaincodeQueryMethod method, ChaincodeOperations operations, SpelExpressionParser expressionParser, QueryMethodEvaluationContextProvider evaluationContextProvider) {
@@ -37,61 +57,170 @@ public class StringBasedChaincodeQuery extends AbstractChaincodeQuery {
 
 	public StringBasedChaincodeQuery(String namedQuery, ChaincodeQueryMethod queryMethod, ChaincodeOperations operations, SpelExpressionParser expressionParser, QueryMethodEvaluationContextProvider evaluationContextProvider) {
 		super(queryMethod, operations);
-		
 		this.query = namedQuery;
-		//this.stringBasedQuery = new StringBasedQuery(namedQuery, new ExpressionEvaluatingParameterBinder(expressionParser, evaluationContextProvider));
+		
+		this.parser = new StringBasedQueryParser(conversionService);
+		this.binder = new StringBasedQueryBinder(query, new ExpressionEvaluatingParameterBinder(expressionParser, evaluationContextProvider));
 	}
 
 	@Override
-	protected String[] createQuery(ParametersParameterAccessor parameterAccessor) {
+	protected String[] createQuery(ParametersParameterAccessor parameterAccessor, Object[] parameterValues) {
 		log.info("args string: {}", query);
 
-		String result = replacePlaceholders(query, parameterAccessor);
-
-		log.info("args result: {}", new Object[] { result });
-		return StringUtils.split(result, QUERY_ARGS_SEPARATOR);
+		if (StringUtils.isNotBlank(query)) {
+			SimpleStatement statement = binder.bindQuery(parameterAccessor, method, parameterValues);
+			
+			String result = parser.replacePlaceholders(statement.getBindableStatement(), statement.getArray());
+			
+			log.info("args result: {}", new Object[] { result });
+			return StringUtils.split(result, QUERY_ARGS_SEPARATOR);
+		}
+		
+		return null;
 	}
-
-	// 替换占位符，将其替换为正确的参数
-	private String replacePlaceholders(String input, ParametersParameterAccessor accessor) {
-		Matcher matcher = PARAMETER_PLACEHOLDER.matcher(input);
+	
+	@Override
+	public Object execute(Object[] parameterValues) {
 		
-		String result = input;
-		while (matcher.find()) {
-			String group = matcher.group();
-			int index = Integer.parseInt(matcher.group(1));
-			result = result.replace(group, getParameterWithIndex(accessor, index));
+		if (method.hasProposalAnnotated()) {
+			log.debug("chaincode proposal request: {}, args: {}", method.getName(), parameterValues);
+		}
+		if (method.hasDeployAnnotated()) {
+			log.debug("chaincode deploy request: {}, args: {}", method.getName(), parameterValues);
+		} 
+		System.err.println(method.getChannelAnnotated());
+		
+		ParametersParameterAccessor accessor = new ParametersParameterAccessor(method.getParameters(), parameterValues);
+		
+		Object[] params = createQuery(accessor, parameterValues);
+		log.info("query stringQueries: {}", new Object[] { params });
+
+		params = Optional.fromNullable(params).or(parameterValues);
+		
+		ResultProcessor processor = method.getResultProcessor().withDynamicProjection(accessor);
+		Class<?> typeToRead = processor.getReturnedType().getTypeToRead();
+		log.info("query result: {}", typeToRead);
+		
+		if (method.hasInstallAnnotated()) {
+			
+			return doInstall(params, processor.getReturnedType());
+		} else if (method.hasInstantiateAnnotated()) {
+			
+			return doInstaantiate(params, processor.getReturnedType());
+		} else if (method.hasUpgradeAnnotated()) {
+			
+			return doUpgrade(params, processor.getReturnedType());
+		} else if (method.hasInvokeAnnotated()) {
+			
+			return doInvoke(params, processor.getReturnedType());
+		} else if (method.hasQueryAnnotated()) {
+			
+			return doQuery(params, processor.getReturnedType());
 		}
 		
-		return result;
+		return null;
 	}
+	
+	private Object doInstall(Object[] parameterValues, ReturnedType returnedType) {
+		Install install = method.getInstallAnnotated();
+		
+		InstallCriteria criteria = new InstallCriteria(method.getCriteria());
+		criteria.setChaincodeUpgradeVersion(install.version());
+		
+		Proposal proposal = method.getProposalAnnotated();
+		if (proposal != null) {
+			criteria.setClientUserContext(getUser(StringUtils.defaultIfBlank(install.clientUser(), proposal.clientUser())));
+			criteria.setProposalWaitTime(proposal.waitTime());
+			criteria.setRequestUser(getUser(proposal.requestUser()));
+			criteria.setSpecificPeers(proposal.specificPeers());
+		}
 
-	// 通过参数占位符获取参数名称
-	@SuppressWarnings("rawtypes")
-	private String getParameterWithIndex(ParametersParameterAccessor accessor, int index) {
-		Object parameter = accessor.getBindableValue(index);
-		if (parameter == null) {
-			return "null";
+		return installOperation(criteria, parameterValues, returnedType, install.chaincodeLocation());
+	} 
+	
+	private Object doInstaantiate(Object[] parameterValues, ReturnedType returnedType) {
+		Instantiate instantiate = method.getInstantiateAnnotated();
+		
+		InstantiateCriteria criteria = new InstantiateCriteria(method.getCriteria());
+
+		criteria.setEndorsementPolicyFile(new File(instantiate.endorsementPolicyFile()));
+		
+		Proposal proposal = method.getProposalAnnotated();
+		if (proposal != null) {
+			criteria.setClientUserContext(getUser(StringUtils.defaultIfBlank(instantiate.clientUser(), proposal.clientUser())));
+			criteria.setProposalWaitTime(proposal.waitTime());
+			criteria.setRequestUser(getUser(proposal.requestUser()));
+			criteria.setSpecificPeers(proposal.specificPeers());
+		}
+
+		Transaction transaction = method.getTransactionAnnotated();
+		if (transaction != null) {
+			criteria.setTransactionsUser(getUser(transaction.user()));
+			criteria.setTransactionWaitTime(transaction.waitTime());
 		}
 		
-		// 参数转换
-		if (conversionService.canConvert(parameter.getClass(), String.class)) {
-			return conversionService.convert(parameter, String.class);
+		return instaantiateOperation(criteria, parameterValues, returnedType, instantiate.func());
+	} 
+	
+	private Object doUpgrade(Object[] parameterValues, ReturnedType returnedType) {
+		Upgrade upgrade = method.getUpgradeAnnotated();
+		
+		UpgradeCriteria criteria = new UpgradeCriteria(method.getCriteria());
+		
+		criteria.setEndorsementPolicyFile(new File(upgrade.endorsementPolicyFile()));
+		
+		Proposal proposal = method.getProposalAnnotated();
+		if (proposal != null) {
+			criteria.setClientUserContext(getUser(StringUtils.defaultIfBlank(upgrade.clientUser(), proposal.clientUser())));
+			criteria.setProposalWaitTime(proposal.waitTime());
+			criteria.setRequestUser(getUser(proposal.requestUser()));
+			criteria.setSpecificPeers(proposal.specificPeers());
+		}
+
+		Transaction transaction = method.getTransactionAnnotated();
+		if (transaction != null) {
+			criteria.setTransactionsUser(getUser(transaction.user()));
+			criteria.setTransactionWaitTime(transaction.waitTime());
 		}
 		
-		if (parameter instanceof Collection) {
-			StringBuilder sb = new StringBuilder();
-			for (Object o : (Collection) parameter) {
-				if (conversionService.canConvert(o.getClass(), String.class)) {
-					sb.append(conversionService.convert(o, String.class));
-				} else {
-					sb.append(o.toString());
-				}
-				sb.append(" ");
-			}
-			return sb.toString().trim();
+		return upgradeOperation(criteria, parameterValues, returnedType, upgrade.func());
+	} 
+	
+	private Object doInvoke(Object[] parameterValues, ReturnedType returnedType) {
+		Invoke invoke = method.getInvokeAnnotated();
+		
+		InvokeCriteria criteria = new InvokeCriteria(method.getCriteria());
+		
+		Proposal proposal = method.getProposalAnnotated();
+		if (proposal != null) {
+			criteria.setClientUserContext(getUser(StringUtils.defaultIfBlank(invoke.clientUser(), proposal.clientUser())));
+			criteria.setProposalWaitTime(proposal.waitTime());
+			criteria.setRequestUser(getUser(proposal.requestUser()));
+			criteria.setSpecificPeers(proposal.specificPeers());
+		}
+
+		Transaction transaction = method.getTransactionAnnotated();
+		if (transaction != null) {
+			criteria.setTransactionsUser(getUser(transaction.user()));
+			criteria.setTransactionWaitTime(transaction.waitTime());
 		}
 		
-		return parameter.toString();
+		return invokeOperation(criteria, parameterValues, returnedType, invoke.func());
+	}
+	
+	private Object doQuery(Object[] parameterValues, ReturnedType returnedType) {
+		Query query = method.getQueryAnnotated();
+		
+		QueryCriteria criteria = new QueryCriteria(method.getCriteria());
+		
+		Proposal proposal = method.getProposalAnnotated();
+		if (proposal != null) {
+			criteria.setClientUserContext(getUser(StringUtils.defaultIfBlank(query.clientUser(), proposal.clientUser())));
+			criteria.setProposalWaitTime(proposal.waitTime());
+			criteria.setRequestUser(getUser(proposal.requestUser()));
+			criteria.setSpecificPeers(proposal.specificPeers());
+		}
+
+		return queryOperation(criteria, parameterValues, returnedType, query.func());
 	}
 }
