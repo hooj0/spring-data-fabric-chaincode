1df94cc4ce7171fb63995303f1bd068ffb65e155
hoojo <hoojo@qq.com>
2018-08-04 09:27:37 +0800

    :sparkles: :bento: :recycle:  Changed chaincode entity information object creator interface

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/creator/ChaincodeEntityInformationCreator.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/creator/ChaincodeEntityInformationCreator.java
@@ -1,11 +1,14 @@
-package io.github.hooj0.springdata.fabric.chaincode.repository.support;
+package io.github.hooj0.springdata.fabric.chaincode.repository.support.creator;
+
+import io.github.hooj0.springdata.fabric.chaincode.repository.support.ChaincodeEntityInformation;
 
 /**
  * ChaincodeEntityInformation 对象创造者接口
+ * @changelog chaincode entity information object creator interface
  * @author hoojo
  * @createDate 2018年7月17日 下午6:51:28
  * @file ChaincodeEntityInformationCreator.java
- * @package io.github.hooj0.springdata.fabric.chaincode.repository.support
+ * @package io.github.hooj0.springdata.fabric.chaincode.repository.support.creator
  * @project spring-data-fabric-chaincode
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
