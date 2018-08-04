ef48f5959af6cbe02204b771e6d31d99bda79f5c
hoojo <hoojo@qq.com>
2018-08-04 09:26:44 +0800

    :sparkles: :meto: Rename `creatro & builder` package, add chaincode fabric fixture resources

---
+++ spring-data-fabric-chaincode/src/test/fixture/integration/e2e-2Orgs/generate.sh
@@ -0,0 +1,211 @@
+#!/bin/bash +x
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#set -e
+#set -uo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+
+CHANNEL_NAME=$1
+CHANNEL_ARTIFACTS_ROOT=$2
+
+CRYPTO_CONFIG_OUTPUT_ROOT=$3
+CRYPTO_CONFIG_FILE=$4
+
+: ${CHANNEL_NAME:="mychannel"}
+: ${CHANNEL_ARTIFACTS_ROOT:="channel-artifacts"}
+: ${CRYPTO_CONFIG_OUTPUT_ROOT:="crypto-config"}
+: ${CRYPTO_CONFIG_FILE:="crypto-config.yaml"}
+
+echo "CHANNEL_NAME: $CHANNEL_NAME"
+echo "CHANNEL_ARTIFACTS_ROOT: $CHANNEL_ARTIFACTS_ROOT"
+echo "CRYPTO_CONFIG_OUTPUT_ROOT: $CRYPTO_CONFIG_OUTPUT_ROOT"
+echo "CRYPTO_CONFIG_FILE: $CRYPTO_CONFIG_FILE"
+
+#version="_v11"
+version=""
+VERSION_DIR="v1.0"
+
+FABRIC_ROOT="/opt/gopath/src/github.com/hyperledger/fabric"
+export FABRIC_ROOT=$FABRIC_ROOT
+export FABRIC_CFG_PATH=$PWD
+
+echo "FABRIC_ROOT: ${FABRIC_ROOT}"
+echo "FABRIC_CFG_PATH: ${FABRIC_CFG_PATH}"
+echo
+
+OS_ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+
+## Using docker-compose template replace private key file names with constants
+function replacePrivateKey () {
+	echo
+	echo "##########################################################"
+	echo "#####         replace certificates  key          #########"
+	echo "##########################################################"
+	
+	ARCH=`uname -s | grep Darwin`
+	echo "ARCH: $ARCH"
+	if [ "$ARCH" == "Darwin" ]; then
+		OPTS="-it"
+	else
+		OPTS="-i"
+	fi
+	echo "OPTS: $OPTS"
+
+    echo
+    echo "==> cp -r script/check-files.tpl.sh script/$VERSION_DIR/check-files.sh"
+    mkdir -pv script/$VERSION_DIR/
+	cp -rv script/check-files.tpl.sh script/$VERSION_DIR/check-files.sh
+
+    CURRENT_DIR=$PWD
+    cd ./$CRYPTO_CONFIG_OUTPUT_ROOT/peerOrganizations/org1.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd $CURRENT_DIR
+	
+    sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" script/$VERSION_DIR/check-files.sh 
+
+    cd ./$CRYPTO_CONFIG_OUTPUT_ROOT/peerOrganizations/org2.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd $CURRENT_DIR
+
+    sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" script/$VERSION_DIR/check-files.sh 
+    echo
+}
+
+## Generates Org certs using cryptogen tool
+function generateCerts (){
+	CRYPTOGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/cryptogen
+
+	if [ -f "$CRYPTOGEN" ]; then
+        echo "Using cryptogen -> $CRYPTOGEN"
+	else
+	    echo "Building cryptogen"
+	    echo "===> make -C $FABRIC_ROOT release"
+	    make -C $FABRIC_ROOT release
+	fi
+
+	echo
+	echo "##########################################################"
+	echo "##### Generate certificates using cryptogen tool #########"
+	echo "##########################################################"
+
+	echo "==> cryptogen generate --config=./$CRYPTO_CONFIG_FILE --output=./$CRYPTO_CONFIG_OUTPUT_ROOT"
+	$CRYPTOGEN generate --config=./$CRYPTO_CONFIG_FILE --output=./$CRYPTO_CONFIG_OUTPUT_ROOT
+	echo
+}
+
+## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
+function generateChannelArtifacts() {
+
+	CONFIGTXGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/configtxgen
+	if [ -f "$CONFIGTXGEN" ]; then
+        echo "Using configtxgen -> $CONFIGTXGEN"
+	else
+	    echo "Building configtxgen"
+	    echo "===> make -C $FABRIC_ROOT release"
+	    make -C $FABRIC_ROOT release
+	fi
+
+    echo
+	echo "##########################################################"
+	echo "#########  Generating Orderer Genesis block ##############"
+	echo "##########################################################"
+	# Note: For some unknown reason (at least for now) the block file can't be
+	# named orderer.genesis.block or the orderer will fail to launch!
+	echo "==> cryptogen -profile TwoOrgsOrdererGenesis${version} -outputBlock ./$CHANNEL_ARTIFACTS_ROOT/genesis.block"
+	$CONFIGTXGEN -profile TwoOrgsOrdererGenesis${version} -outputBlock ./$CHANNEL_ARTIFACTS_ROOT/genesis.block
+	
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	echo "==> cryptogen -profile TwoOrgsChannel${version} -outputCreateChannelTx ./$CHANNEL_ARTIFACTS_ROOT/$CHANNEL_NAME.tx -channelID $CHANNEL_NAME"
+	$CONFIGTXGEN -profile TwoOrgsChannel${version} -outputCreateChannelTx ./$CHANNEL_ARTIFACTS_ROOT/$CHANNEL_NAME.tx -channelID $CHANNEL_NAME
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	echo "==> cryptogen -profile TwoOrgsChannel${version} -outputCreateChannelTx ./$CHANNEL_ARTIFACTS_ROOT/${CHANNEL_NAME}2.tx -channelID ${CHANNEL_NAME}2"
+	$CONFIGTXGEN -profile TwoOrgsChannel${version} -outputCreateChannelTx ./$CHANNEL_ARTIFACTS_ROOT/${CHANNEL_NAME}2.tx -channelID ${CHANNEL_NAME}2
+}
+
+function skip() {
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "#################################################################"
+	echo "==> cryptogen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./$CHANNEL_ARTIFACTS_ROOT/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./$CHANNEL_ARTIFACTS_ROOT/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org2MSP   ##########"
+	echo "#################################################################"
+	echo "==> cryptogen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./$CHANNEL_ARTIFACTS_ROOT/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./$CHANNEL_ARTIFACTS_ROOT/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+	echo
+}
+
+function cleanChannelArtifacts() {
+
+    echo
+	echo "#################################################################"
+	echo "#######            clean channel artifacts             ##########"
+	echo "#################################################################"
+
+	
+	echo "==> rm -rf ./$VERSION_DIR/"
+    [ -n $VERSION_DIR ] && [ -d "./$VERSION_DIR" ] && rm -rf ./$VERSION_DIR/*
+
+	echo "==> rm -rf ./channel-artifacts/* ./crypto-config/* ./script/$VERSION_DIR"
+    rm -rfv ./channel-artifacts/* ./crypto-config/* ./script/$VERSION_DIR
+    
+    echo
+}
+
+function createChannelArtifactsDir() {
+
+    echo
+	echo "#################################################################"
+	echo "#######            create channel artifacts            ##########"
+	echo "#################################################################"
+
+    echo "==> mkdir ./channel-artifacts"
+	[ ! -d "./channel-artifacts" ] && mkdir ./channel-artifacts 
+
+    echo "==> mkdir ./crypto-config"
+	[ ! -d "./crypto-config" ] && mkdir ./crypto-config 
+    
+    echo
+}
+
+function mergeArtifactsCryptoDir() {
+	echo
+	echo "#################################################################"
+	echo "#######            merge channel artifacts             ##########"
+	echo "#################################################################"
+
+	echo "==> mv ./channel-artifacts ./$VERSION_DIR/"
+    mv -v ./channel-artifacts ./$VERSION_DIR/
+
+	echo "==> mv ./crypto-config ./$VERSION_DIR/"
+    mv -v ./crypto-config ./$VERSION_DIR/
+
+    echo
+}
+
+echo  "shell run example: $ ./generate.sh tx_channel channel_out crypto_out crypto_config_extend.yaml"
+
+cleanChannelArtifacts
+createChannelArtifactsDir
+generateCerts
+replacePrivateKey
+generateChannelArtifacts
+#skip
+mergeArtifactsCryptoDir
+
---
+++ spring-data-fabric-chaincode/src/test/fixture/integration/e2e-2Orgs/script/check-files.tpl.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+
+ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_CA_TLS=
+
+V11_IDENTITIES_ALLOWREMOVE=--cfg.identities.allowremove
+V11_AFFILIATIONS_ALLOWREMOVE=--cfg.affiliations.allowremove
+
+function check_ca0() {
+	ls -l /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+	ls -l /etc/hyperledger/fabric-ca-server-config/CA1_PRIVATE_KEY
+	ls -l /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+	ls -l /etc/hyperledger/fabric-ca-server-config/CA1_PRIVATE_KEY
+	
+	fabric-ca-server start -n ca0 ${V11_IDENTITIES_ALLOWREMOVE} ${V11_AFFILIATIONS_ALLOWREMOVE} --registry.maxenrollments -1 --ca.certfile /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem --ca.keyfile /etc/hyperledger/fabric-ca-server-config/CA1_PRIVATE_KEY -b admin:adminpw ${ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_CA_TLS} --tls.certfile /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem --tls.keyfile /etc/hyperledger/fabric-ca-server-config/CA1_PRIVATE_KEY -d
+}
+
+
+function check_ca1() {
+	ls -l /etc/hyperledger/fabric-ca-server-config/ca.org2.example.com-cert.pem
+	ls -l /etc/hyperledger/fabric-ca-server-config/CA2_PRIVATE_KEY
+	
+	ls -l /etc/hyperledger/fabric-ca-server-config/ca.org2.example.com-cert.pem
+	ls -l /etc/hyperledger/fabric-ca-server-config/CA2_PRIVATE_KEY
+	
+	fabric-ca-server start --registry.maxenrollments -1 --ca.certfile /etc/hyperledger/fabric-ca-server-config/ca.org2.example.com-cert.pem --ca.keyfile /etc/hyperledger/fabric-ca-server-config/CA2_PRIVATE_KEY -b admin:adminpw ${ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_CA_TLS} --tls.certfile /etc/hyperledger/fabric-ca-server-config/ca.org2.example.com-cert.pem --tls.keyfile /etc/hyperledger/fabric-ca-server-config/CA2_PRIVATE_KEY -d
+}
+
+function check_orderer() {
+	ls -l /etc/hyperledger/configtx/genesis.block
+	ls -l /etc/hyperledger/msp/orderer/msp
+	ls -l /etc/hyperledger/msp/orderer/tls/server.key
+	ls -l /etc/hyperledger/msp/orderer/tls/server.crt
+	ls -l /etc/hyperledger/msp/orderer/tls/ca.crt
+	
+	ls -l /etc/hyperledger/msp/peerOrg1/msp/tlscacerts/tlsca.org1.example.com-cert.pem /etc/hyperledger/msp/peerOrg2/msp/tlscacerts/tlsca.org2.example.com-cert.pem
+	
+	orderer
+}
+
+function check_peer0_org1() {
+	ls -l /etc/hyperledger/msp/peer/msp/tlscacerts/tlsca.org1.example.com-cert.pem
+	
+	ls -l /etc/hyperledger/msp/peer/msp
+	ls -l /etc/hyperledger/msp/peer/tls/server.crt
+	ls -l /etc/hyperledger/msp/peer/tls/server.key
+	ls -l /etc/hyperledger/msp/peer/tls/ca.crt
+	
+	peer node start
+	
+	exit 0
+}
+
+function check_peer1_org1() {
+	ls -l /etc/hyperledger/msp/peer/msp/tlscacerts/tlsca.org1.example.com-cert.pem
+	
+	ls -l /etc/hyperledger/msp/peer/msp
+	ls -l /etc/hyperledger/msp/peer/tls/server.crt
+	ls -l /etc/hyperledger/msp/peer/tls/server.key
+	ls -l /etc/hyperledger/msp/peer/tls/ca.crt
+	
+	peer node start
+	
+	exit 0
+}
+
+
+function check_peer0_org2() {
+	ls -l /etc/hyperledger/msp/peer/msp/tlscacerts/tlsca.org2.example.com-cert.pem
+	
+	ls -l /etc/hyperledger/msp/peer/msp
+	ls -l /etc/hyperledger/msp/peer/tls/server.crt
+	ls -l /etc/hyperledger/msp/peer/tls/server.key
+	ls -l /etc/hyperledger/msp/peer/tls/ca.crt
+	
+	peer node start
+	
+	exit 0
+}
+
+function check_peer1_org2() {
+	ls -l /etc/hyperledger/msp/peer/msp/tlscacerts/tlsca.org2.example.com-cert.pem
+	
+	ls -l /etc/hyperledger/msp/peer/msp
+	ls -l /etc/hyperledger/msp/peer/tls/server.crt
+	ls -l /etc/hyperledger/msp/peer/tls/server.key
+	ls -l /etc/hyperledger/msp/peer/tls/ca.crt
+	
+	peer node start
+	
+	exit 0
+}
+
+op=$1
+
+function main() {
+	echo "first param: $op"
+	
+	case $op in
+	    ca0)  
+	    	echo '-------check_ca0---------'
+	    	check_ca0
+	    ;;
+	    ca1)  
+	    	echo '-------check_ca1---------'
+	    	check_ca1
+	    ;;
+	    orderer)  
+	    	echo '-------check_orderer---------'
+	    	check_orderer
+	    ;;
+	    4)  
+	    	echo '-------check_peer0_org1---------'
+	    	check_peer0_org1
+	    ;;
+	    5)  
+	    	echo '-------check_peer1_org1---------'
+	    	check_peer1_org1
+	    ;;
+	    6)  
+	    	echo '-------check_peer0_org2---------'
+	    	check_peer0_org2
+	    ;;
+	    7)  
+	    	echo '-------check_peer1_org2---------'
+	    	check_peer1_org2
+	    ;;
+	esac
+}
+
+main
