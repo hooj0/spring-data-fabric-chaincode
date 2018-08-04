a274b7b5b0df4a15fa453f7baf7b435440f3cc3b
hoojo <hoojo@qq.com>
2018-08-04 09:27:20 +0800

    :sparkles: :bento: :recycle:  Changed Smart contract request proposal, issued request before all trading actions

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Proposal.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/annotations/repository/Proposal.java
@@ -7,13 +7,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.core.annotation.AliasFor;
 import org.springframework.data.annotation.QueryAnnotation;
 
 import io.github.hooj0.springdata.fabric.chaincode.enums.ProposalType;
 
 /**
  * 智能合约 请求提议，在所有交易动作之前的发出的请求
+ * @changelog Smart contract request proposal, issued request before all trading actions
  * @author hoojo
  * @createDate 2018年7月18日 上午9:54:19
  * @file Proposal.java
@@ -33,7 +33,6 @@ public @interface Proposal {
 	String value() default "";
 	
 	/** 执行Chaincode智能合约的参数，支持占位符或spel表达式，默认取参数列表  */
-	@AliasFor(annotation = Proposal.class, attribute = "args")
 	String[] args() default {};
 	
 	ProposalType type() default ProposalType.INVOKE;
