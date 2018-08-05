766cf9a48bbe26f94ec717586508a7b31d60ea7e
hoojo <hoojo@qq.com>
2018-08-05 10:01:20 +0800

    :sparkles: :bento: :recycle:  Changed annotationed repository test units

--- spring-data-fabric-chaincode/src/test/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/AnnotationedRepositoryTests.java
+++ spring-data-fabric-chaincode/src/test/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/AnnotationedRepositoryTests.java
@@ -1,11 +1,12 @@
 package io.github.hooj0.springdata.fabric.chaincode.repository.support;
 
-import java.io.File;
-import java.io.InputStream;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import org.hyperledger.fabric.sdk.BlockEvent.TransactionEvent;
 import org.hyperledger.fabric.sdk.TransactionRequest.Type;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,25 +21,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.convert.CustomConversions;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Entity;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Field;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Transient;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Chaincode;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Channel;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Invoke;
-import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Proposal;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Query;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Serialization;
 import io.github.hooj0.springdata.fabric.chaincode.config.AbstractChaincodeConfiguration;
 import io.github.hooj0.springdata.fabric.chaincode.core.ChaincodeOperations;
 import io.github.hooj0.springdata.fabric.chaincode.core.convert.ChaincodeConverter;
 import io.github.hooj0.springdata.fabric.chaincode.core.mapping.SimpleChaincodeMappingContext;
 import io.github.hooj0.springdata.fabric.chaincode.core.support.ChaincodeTemplate;
+import io.github.hooj0.springdata.fabric.chaincode.enums.SerializationMode;
 import io.github.hooj0.springdata.fabric.chaincode.repository.ChaincodeRepository;
 import io.github.hooj0.springdata.fabric.chaincode.repository.config.EnableChaincodeRepositories;
+import io.github.hooj0.springdata.fabric.chaincode.repository.support.creator.ProposalBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -76,11 +81,13 @@ public class AnnotationedRepositoryTests {
 		}
 	}
 	
-	@Autowired @Qualifier("goRepo")
-	private GoRepo goRepo;
-	
-	@Autowired @Qualifier("goRepo")
+	@Autowired 
+	@Qualifier("myRepo")
 	private MyRepo myRepo;
+	
+	@Autowired 
+	@Qualifier("goRepo")
+	private GoRepo goRepo;
 	
 	@Autowired
 	@Qualifier("nodeRepo")
@@ -100,21 +107,75 @@ public class AnnotationedRepositoryTests {
 		Assert.assertNotNull("null converter", converter);
 		Assert.assertNotNull("null conversion", conversion);
 		
-		Assert.assertNotNull("null goRepo", goRepo);
 		Assert.assertNotNull("null myRepo", myRepo);
+		Assert.assertNotNull("null goRepo", goRepo);
+		Assert.assertNotNull("null nodeRepo", nodeRepo);
 	}
 	
 	@Test
-	public void testInvoke() {
-		//System.out.println(myRepo.invoke("move", "2", "3"));
-		//System.out.println(myRepo.query("2223333"));
-		//goRepo.installChaincode("src/gochaincode");
-		//nodeRepo.installChaincode("src/gochaincode22222");
+	public void testBasicInvoke() {
+		ProposalBuilder.InvokeProposal invokeProposal = ProposalBuilder.invoke();
+		invokeProposal.clientUser("user1");
+		
+		System.out.println(myRepo.getOrganization());
+		System.out.println(myRepo.getOrganization().getUser("user1"));
+		System.out.println(operations.getOrganization(myRepo.getCriteria()).getUser("user1"));
+		
+		System.out.println(myRepo.invoke(invokeProposal, "move", "a", "b", 5));
+	}
+	
+	@Test
+	public void testBasicQuery() {
+		try {
+			ProposalBuilder.QueryProposal proposal = ProposalBuilder.query();
+			proposal.clientUser("user1");
+			
+			System.out.println(myRepo.query(proposal, "query", "a"));
+			System.out.println(myRepo.query(proposal, "query", "b"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testInvoke() throws InterruptedException, ExecutionException {
+		//goRepo.move(10);
+		//goRepo.move("b", "a", 10);
+		//goRepo.moveMethod(5);
+		
+		//System.out.println(goRepo.moveString("b", "a", 20));
+		//System.out.println(goRepo.moveResult(10));
+		//System.out.println(goRepo.moveFuture(10).get());
+		System.out.println(goRepo.moveEvent(10));
+	}
+	
+	@Test
+	public void testQuery() {
+		//System.out.println(goRepo.query("a"));
+		//System.out.println(goRepo.query());
+		
+		//System.out.println(goRepo.queryCustom());
+		//System.out.println(goRepo.queryExprssion("a"));
+		//System.out.println(goRepo.queryExprssion2("x", "y", "b"));
+		
+		//System.out.println(goRepo.queryResult());
+		//System.out.println(goRepo.queryInt());
+		
+		// goRepo.queryNull();
+		//System.out.println(goRepo.queryFor());
+		
+		Person p = new Person(1);
+		p.setName("b");
+		p.setDate(new Date());
+		p.setVersion(1223L);
+		//System.out.println(goRepo.queryForExpression(p));
+		
+		//System.out.println(goRepo.queryForJSON(p));
+		System.out.println(goRepo.queryForJSONOPerson(p));
 	}
 	
 	@Test
 	public void testInstallChaincode() {
-		goRepo.installChaincode("src/gochaincode");
 	}
 	
 	@Entity
@@ -123,32 +184,83 @@ public class AnnotationedRepositoryTests {
 	@RequiredArgsConstructor
 	static class Person {
 		@Id private final int number;
-		
 		@Field(transientAlias = "personName") String name;
-		
 		@Transient(alias = "oldVersion") Long version;
-		
 		@Transient Date date;
 	}
 	
-	@Channel(name = "channel_0.1", org = "org2")
-	@Chaincode(name = "mycc", type = Type.GO_LANG, version = "1.1", path = "github.com/example_cc")
+	@Channel(name = "mychannel", org = "peerOrg1")
+	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
+	@Repository("myRepo")
 	interface MyRepo extends ChaincodeRepository<Person> {
 	}
 	
-	@Chaincode(channel = "mychannel_2", name = "mycc", type = Type.GO_LANG, version = "1.1", path = "github.com/example_cc")
+	@Channel(name = "mychannel", org = "peerOrg1")
+	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
 	@Repository("goRepo")
 	interface GoRepo extends MyRepo {
 		
-		@Proposal("select * from a where a = ?0")
-		@Channel(name = "mychannel_123", org = "org1")
-		public void installChaincode(String ccPath);
+		@Query(clientUser = "user1")
+		String query(String account);
 		
-		@Query("a,b,c,e")
-		public void installChaincode2(File ccFile);
+		@Query(clientUser = "user1", args = "b")
+		String query();
+		
+		@Query(clientUser = "user1", func = "query", args = "b")
+		String queryCustom();
+		
+		@Query(clientUser = "user1", func = "query", args = ":account")
+		String queryExprssion(@Param("account") String account);
+		
+		@Query(clientUser = "user1", func = "query", args = "?2")
+		String queryExprssion2(String param, String param2, String account);
+		
+		@Query(clientUser = "user1", func = "query", args = "b")
+		ResultSet queryResult();
+		
+		@Query(clientUser = "user1", func = "query", args = "b")
+		int queryInt();
+		
+		@Query(clientUser = "user1", func = "query", args = "b")
+		void queryNull();
+		
+		@Query(clientUser = "user1", func = "query", args = "b")
+		Person queryFor();
+		
+		@Query(clientUser = "user1", func = "query", args = ":#{#person.name}")
+		Person queryForExpression(@Param("person") Person p);
+		
+		@Query(clientUser = "user1", func = "query")
+		@Serialization
+		Person queryForJSON(Person p);
+		
+		@Query(clientUser = "user1", func = "query", args = ":#{#person.name}")
+		@Serialization(SerializationMode.DESERIALIZE)
+		Person queryForJSONOPerson(@Param("person") Person p);
+		
+		
+		
+		
+		@Invoke(clientUser = "user1", args = { "a", "b", "?0" })
+		void move(int amount);
+		
+		@Invoke(clientUser = "user1")
+		void move(String from, String to, int amount);
+		
+		@Invoke(clientUser = "user1", func = "move", args = { "a", "b", "?0" })
+		void moveMethod(int amount);
+		
+		@Invoke(clientUser = "user1", func = "move")
+		String moveString(String from, String to, int amount);
+		
+		@Invoke(clientUser = "user1", func = "move", args = { "a", "b", "?0" })
+		ResultSet moveResult(int amount);
 
-		@Invoke("xyz")
-		public void installChaincode3(InputStream ccStream);
+		@Invoke(clientUser = "user1", func = "move", args = { "a", "b", "?0" })
+		CompletableFuture<TransactionEvent> moveFuture(int amount);
+		
+		@Invoke(clientUser = "user1", func = "move", args = { "a", "b", "?0" })
+		TransactionEvent moveEvent(int amount);
 	}
 	
 	@Chaincode(channel = "mychannel_3", name = "mycc", type = Type.NODE, version = "1.1")
