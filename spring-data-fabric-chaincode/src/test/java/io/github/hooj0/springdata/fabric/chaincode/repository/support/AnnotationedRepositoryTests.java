523f3070fd3499664e9d66318f440f5864f16da2
hoojo <hoojo@qq.com>
2018-08-06 18:00:36 +0800

    :sparkles: :bento: :recycle:  Changed annotationed repository test units

--- spring-data-fabric-chaincode/src/test/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/AnnotationedRepositoryTests.java
+++ spring-data-fabric-chaincode/src/test/java/io/github/hooj0/springdata/fabric/chaincode/repository/support/AnnotationedRepositoryTests.java
@@ -7,6 +7,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.hyperledger.fabric.sdk.BlockEvent.TransactionEvent;
+import org.hyperledger.fabric.sdk.ChaincodeID;
 import org.hyperledger.fabric.sdk.TransactionRequest.Type;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,16 +27,20 @@ import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import io.github.hooj0.fabric.sdk.commons.core.ChaincodeDeployOperations;
 import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Entity;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Field;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.Transient;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Chaincode;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Channel;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Install;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Instantiate;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Invoke;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Proposal;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Query;
 import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Serialization;
+import io.github.hooj0.springdata.fabric.chaincode.annotations.repository.Upgrade;
 import io.github.hooj0.springdata.fabric.chaincode.config.AbstractChaincodeConfiguration;
 import io.github.hooj0.springdata.fabric.chaincode.core.ChaincodeOperations;
 import io.github.hooj0.springdata.fabric.chaincode.core.convert.ChaincodeConverter;
@@ -68,7 +73,7 @@ import lombok.extern.slf4j.Slf4j;
 public class AnnotationedRepositoryTests {
 
 	@Configuration
-	@EnableChaincodeRepositories(basePackageClasses = GoRepo.class,
+	@EnableChaincodeRepositories(basePackageClasses = TransactionRepo.class,
 		considerNestedRepositories = true, 
 		includeFilters = @Filter(pattern = ".*Repo", type = FilterType.REGEX))
 	public static class Config extends AbstractChaincodeConfiguration {
@@ -88,8 +93,20 @@ public class AnnotationedRepositoryTests {
 	private MyRepo myRepo;
 	
 	@Autowired 
-	@Qualifier("goRepo")
-	private GoRepo goRepo;
+	@Qualifier("txRepo")
+	private TransactionRepo txRepo;
+	
+	@Autowired 
+	private DeployRepo deployRepo;
+	
+	@Autowired
+	private UpgradeDeployRepo upgradeRepo;
+	
+	@Autowired
+	private ProposalRepo proposalRepo;
+
+	@Autowired
+	private NewProposalRepo proposalRepoNew;
 	
 	@Autowired
 	@Qualifier("nodeRepo")
@@ -110,7 +127,7 @@ public class AnnotationedRepositoryTests {
 		Assert.assertNotNull("null conversion", conversion);
 		
 		Assert.assertNotNull("null myRepo", myRepo);
-		Assert.assertNotNull("null goRepo", goRepo);
+		Assert.assertNotNull("null txRepo", txRepo);
 		Assert.assertNotNull("null nodeRepo", nodeRepo);
 	}
 	
@@ -148,7 +165,7 @@ public class AnnotationedRepositoryTests {
 		//System.out.println(goRepo.moveString("b", "a", 20));
 		//System.out.println(goRepo.moveResult(10));
 		//System.out.println(goRepo.moveFuture(10).get());
-		System.out.println(goRepo.moveEvent(10));
+		System.out.println(txRepo.moveEvent(10));
 	}
 	
 	@Test
@@ -163,26 +180,114 @@ public class AnnotationedRepositoryTests {
 		//System.out.println(goRepo.queryResult());
 		//System.out.println(goRepo.queryInt());
 		
-		// goRepo.queryNull();
+		//goRepo.queryNull();
 		//System.out.println(goRepo.queryFor());
 		
 		Person p = new Person(1);
 		p.setName("b");
 		p.setDate(new Date());
 		p.setVersion(1223L);
-		//System.out.println(goRepo.queryForExpression(p));
+		System.out.println(txRepo.queryForExpression(p));
 		
 		//System.out.println(goRepo.queryForJSON(p));
-		System.out.println(goRepo.queryForJSONOPerson(p));
+		//System.out.println(goRepo.queryForJSONOPerson(p));
 	}
 	
 	@Test
-	public void testProposalQuery() {
-		System.out.println(goRepo.queryProposal());
+	public void testProposalDeploy() throws Exception {
+		
+		ChaincodeDeployOperations deployOperations = proposalRepo.getChaincodeDeployOperations();
+		
+		// install chaincode
+		if (!deployOperations.checkInstallChaincode(proposalRepo.getCriteria().getChaincodeID())) {
+			proposalRepo.installMyCC();		// 依赖 Configuration 中配置的 getChaincodeRootPath 路径	
+		}
+		
+		// instantiate chaincode
+		if (!deployOperations.checkInstantiatedChaincode(proposalRepo.getCriteria().getChaincodeID())) {
+			proposalRepo.instantiateMyCC(1000, 2000);	// 依赖 Configuration 中配置的 getEndorsementPolicyFilePath 路径
+		}
+		
+		ChaincodeID old = proposalRepo.getCriteria().getChaincodeID();
+		ChaincodeID newId = ChaincodeID.newBuilder().setName(old.getName())
+				.setPath("gocc/sample_11")
+				.setVersion("v11.1")
+				.build();
+		
+		
+		// upgrade chaincode
+		if (!deployOperations.checkChaincode(old, proposalRepo.getOrganization())) {
+			throw new AssertionError(old + " Not installed and instantiated");
+		}
+		if (deployOperations.checkChaincode(newId, proposalRepo.getOrganization())) {
+			throw new AssertionError(newId + " Already installed or instantiated");
+		}
+		
+		if (!deployOperations.checkInstallChaincode(newId)) {
+			proposalRepo.installNewVersionMyCC();
+		} else {
+			System.err.println("Successful Installed：" + newId);
+		}
+		
+		if (deployOperations.checkInstallChaincode(newId) && !deployOperations.checkInstantiatedChaincode(newId)) {
+			ResultSet rs = proposalRepoNew.upgradeMyCC();		// 依赖 Configuration 中配置的 getEndorsementPolicyFilePath 路径
+			System.out.println(rs);
+		} else {
+			System.err.println("Successful Instantiated：" + newId);
+		}
+		
+		proposalRepoNew.query();
 	}
 	
 	@Test
-	public void testInstallChaincode() {
+	public void testProposalTransaction() throws Exception {
+		
+		// query
+		System.out.println(proposalRepo.queryProposal());
+		
+		// invoke
+		System.out.println(proposalRepo.invokeProposal(10));
+		
+		// invoke
+		System.out.println(proposalRepo.move(20));
+		
+		// query
+		System.out.println(proposalRepo.query());
+	}
+	
+	@Test
+	public void testDeployChaincode() throws Exception {
+		
+		ChaincodeDeployOperations deployOperations = deployRepo.getChaincodeDeployOperations();
+		
+		// install chaincode
+		if (!deployOperations.checkInstallChaincode(deployRepo.getCriteria().getChaincodeID())) {
+			deployRepo.install();		
+		}
+		
+		// instantiate chaincode
+		if (!deployOperations.checkInstantiatedChaincode(deployRepo.getCriteria().getChaincodeID())) {
+			deployRepo.instantiate(300, 500);	// 依赖 Configuration 中配置的 getEndorsementPolicyFilePath 路径
+		}
+	}
+	
+	@Test
+	public void testDeployNewChaincode() throws Exception {
+		
+		ChaincodeDeployOperations deployOperations = upgradeRepo.getChaincodeDeployOperations();
+		
+		ChaincodeID id = upgradeRepo.getCriteria().getChaincodeID();
+		id = ChaincodeID.newBuilder().setName(id.getName()).setPath(id.getPath()).setVersion("v11.2").build();
+		
+		// install new chaincode
+		if (!deployOperations.checkInstallChaincode(id)) {
+			deployRepo.installNewVersion();
+		}
+		
+		// upgrade chaincode
+		if (!deployOperations.checkInstantiatedChaincode(upgradeRepo.getCriteria().getChaincodeID())) {
+			upgradeRepo.upgrade();
+		}
 	}
 	
 	@Entity
@@ -197,15 +302,15 @@ public class AnnotationedRepositoryTests {
 	}
 	
 	@Channel(name = "mychannel", org = "peerOrg1")
-	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
+	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "10", path = "github.com/example_cc")
 	@Repository("myRepo")
 	interface MyRepo extends ChaincodeRepository<Person> {
 	}
 	
 	@Channel(name = "mychannel", org = "peerOrg1")
 	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
-	@Repository("goRepo")
-	interface GoRepo extends MyRepo {
+	@Repository("txRepo")
+	interface TransactionRepo extends MyRepo {
 		
 		@Query(clientUser = "user1")
 		String query(String account);
@@ -247,10 +352,6 @@ public class AnnotationedRepositoryTests {
 		
 		
 		
-		@Proposal(clientUser = "user1", func = "query", args = "b", type = ProposalType.QUERY)
-		String queryProposal();
-		
-		
 		@Invoke(clientUser = "user1", args = { "a", "b", "?0" })
 		void move(int amount);
 		
@@ -273,9 +374,69 @@ public class AnnotationedRepositoryTests {
 		TransactionEvent moveEvent(int amount);
 	}
 	
-	@Chaincode(channel = "mychannel_3", name = "mycc", type = Type.NODE, version = "1.1")
+	@Channel(name = "mychannel", org = "peerOrg1")
+	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
+	interface DeployRepo extends ChaincodeRepository<Person> {
+		
+		@Install(clientUser = "admin", chaincodeLocation = "gocc/sample_11")
+		void install();
+		
+		@Instantiate(clientUser = "admin", endorsementPolicyFile = "chaincode-endorsement-policy.yaml", func = "init", args = { "a", "?0", "b", "?1" })
+		TransactionEvent instantiate(int aAmount, int bAmount);
+		
+		@Install(clientUser = "admin", chaincodeLocation = "gocc/sample_11", version = "v11.2")
+		void installNewVersion();
+	}
+	
+	@Chaincode(channel = "mychannel", org = "peerOrg1", name = "example_cc_go", type = Type.GO_LANG, version = "v11.2", path = "github.com/example_cc")
+	interface UpgradeDeployRepo extends ChaincodeRepository<Person> {
+		
+		@Upgrade(clientUser = "admin", endorsementPolicyFile = "chaincode-endorsement-policy.yaml", func = "init")
+		TransactionEvent upgrade();
+	}
+	
+	@Channel(name = "mychannel", org = "peerOrg1")
+	@Chaincode(name = "example_cc_go", type = Type.GO_LANG, version = "11", path = "github.com/example_cc")
+	interface ProposalRepo extends ChaincodeRepository<Person> {
+		
+		@Proposal(type = ProposalType.INSTALL, clientUser = "admin", waitTime = 2000L)
+		void installMyCC();
+		
+		@Proposal(type = ProposalType.INSTANTIATE, clientUser = "admin", waitTime = 5000L, func = "init", args = { "a", "?0", "b", "?1" })
+		ResultSet instantiateMyCC(int aAmount, int bAmount);
+		
+		@Proposal(type = ProposalType.INSTALL, clientUser = "admin", waitTime = 2000L)
+		@Install(chaincodeLocation = "gocc/sample_11", version = "v11.1")	// setter new version & chaincode location
+		void installNewVersionMyCC();
+		
+		
+		@Proposal(type = ProposalType.QUERY, clientUser = "user1", func = "query", args = "b")
+		String queryProposal();
+		
+		@Proposal(type = ProposalType.QUERY, clientUser = "user1", args = "b")
+		String query();
+		
+		@Proposal(type = ProposalType.INVOKE, clientUser = "user1", func = "move", args = { "b", "a", "?0" })
+		String invokeProposal(int amount);
+
+		@Proposal(type = ProposalType.INVOKE, clientUser = "user1", args = { "a", "b", "?0" })
+		String move(int amount);
+	}
+	
+	// used new chaincode version & path
+	@Chaincode(channel = "mychannel", org = "peerOrg1", name = "example_cc_go", type = Type.GO_LANG, version = "v11.1", path = "github.com/example_cc")
+	interface NewProposalRepo extends ChaincodeRepository<Person> {
+		
+		@Proposal(type = ProposalType.UPGRADE, clientUser = "admin", waitTime = 50000L, func = "init")
+		ResultSet upgradeMyCC();
+		
+		@Proposal(type = ProposalType.QUERY, clientUser = "user1", args = "b")
+		String query();
+	}
+	
+	//@Chaincode(channel = "mychannel_3", name = "mycc", type = Type.NODE, version = "1.1")
 	@Repository("nodeRepo")
-	interface NodeRepo extends GoRepo {
+	interface NodeRepo extends TransactionRepo {
 		
 	}
 	
