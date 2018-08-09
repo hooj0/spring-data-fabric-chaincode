db01aa5717c60a0c458172e1fa05a34d7d50ec1e
hoojo <hoojo@qq.com>
2018-08-09 19:39:00 +0800

    :sparkles: :bento: :tada:  Init add spring data fabric chaincode basic abstract entity, other entity objects need to inherit this entity

---
+++ spring-data-fabric-chaincode-domain/src/main/java/io/github/hooj0/springdata/fabric/chaincode/domain/AbstractEntity.java
@@ -0,0 +1,32 @@
+package io.github.hooj0.springdata.fabric.chaincode.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import io.github.hooj0.springdata.fabric.chaincode.annotations.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * spring data fabric chaincode basic abstract entity, other entity objects need to inherit this entity
+ * @author hoojo
+ * @createDate 2018年7月17日 下午5:18:44
+ * @file BaseEntity.java
+ * @package io.github.hooj0.springdata.fabric.chaincode.entity
+ * @project spring-data-fabric-chaincode
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@Entity
+@Getter
+@Setter
+public abstract class AbstractEntity {
+	
+	private String transactionId;
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+}
