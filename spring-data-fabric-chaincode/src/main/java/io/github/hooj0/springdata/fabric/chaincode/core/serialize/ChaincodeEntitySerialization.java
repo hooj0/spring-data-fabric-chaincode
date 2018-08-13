c406f6c593e0369120c8730b72320ca54fc6f6ce
hoojo <hoojo@qq.com>
2018-08-13 19:06:23 +0800

    :sparkles: :bento: :recycle:  Changed chaincode operation object grouping and unmarshalling/sequence and deserialization between strings and entity objects

--- spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/serialize/ChaincodeEntitySerialization.java
+++ spring-data-fabric-chaincode/src/main/java/io/github/hooj0/springdata/fabric/chaincode/core/serialize/ChaincodeEntitySerialization.java
@@ -1,5 +1,7 @@
 package io.github.hooj0.springdata.fabric.chaincode.core.serialize;
 
+import io.github.hooj0.springdata.fabric.chaincode.repository.query.ChaincodeQueryMethod;
+
 /**
  * Chaincode 操作对象 字符串与实体对象之间的编组和解组/序列和反序列化
  * @changelog chaincode operation object grouping and unmarshalling/sequence and deserialization between strings and entity objects
@@ -26,9 +28,9 @@ public interface ChaincodeEntitySerialization {
 	 * 将字符串反序列成对象
 	 * @author hoojo
 	 * @createDate 2018年7月22日 下午1:51:47
-	 * @param value deserialize target class
 	 * @param value json string
+	 * @param value deserialize target ChaincodeQueryMethod
 	 * @return deserialize value
 	 */
-	public <T> T deserialize(Class<T> clazz, String json);
+	public <T> T deserialize(String json, ChaincodeQueryMethod method);
 }
